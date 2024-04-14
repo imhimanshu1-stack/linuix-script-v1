ed8de2b0a26feb34a4208353b5f0fcd39283f7e2
Himanshu Verma | DevOps | Full Stack Developer <79570418+imhimanshu1-stack@users.noreply.github.com>
2024-04-14 02:34:24 +0100

    Update crxfile.sh

--- crxfile.sh
+++ crxfile.sh
@@ -3,11 +3,5 @@
 # Navigate to the /home directory
 cd /home
 
-# Download Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-# Install Google Chrome
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
-
 # Install OBS Studio
 sudo apt install obs-studio -y
